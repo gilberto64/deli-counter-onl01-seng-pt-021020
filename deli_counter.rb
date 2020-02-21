fb822c9d97638c59713b90bf92e1df91f8f0f3fc
Gilberto Ocampo <ocampo@gate.net>
2020-02-21 14:12:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    phrase_arr = array.each_with_index.map {|name,idx| 
+      "#{idx + 1}. #{name}"}
+    puts "The line is currently: #{phrase_arr.join (' ')}"
+  end 
+end
+
+def take_a_number(arr,name) 
+  puts"Welcome, #{name}. You are number #{arr.length + 1} in line."
+  arr << name 
+end
+ 
+ def now_serving (arr2)
+  if arr2.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    arr2.each {|x| puts "Currently serving #{x}."}
+  end
+    
+end 
